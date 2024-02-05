9215b75fc910b586de9fb1fda729ffccd2624946
Umair <umairnadeem.baloch@gmail.com>
2024-02-05 17:58:45 +0500

    dockerized psql db configurations for application

---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+Sample test cases
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class ClassTests(SimpleTestCase):
+    def test_add(self):
+        res = calc.add(3, 8)
+        self.assertEqual(res, 11)
+
+    def test_subtract(self):
+        res = calc.subtract(5, 11)
+        self.assertEqual(res, 6)
